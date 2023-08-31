42279431cecca87121ebbddbb2f23af16cee9c7b
Silas Emanuel <emanusilas@gmail.com>
2023-08-30 23:26:18 -0300

    testing with k6.

---
+++ aulasiniciais/1-inicio.js
@@ -0,0 +1,9 @@
+//inicialização
+import sleep from 'k6';
+
+//configuração passando os VU´S e duração
+export const options = {
+    vus: 1,
+    duration: '10s'
+}
+
---
+++ aulasiniciais/2-check.js
@@ -0,0 +1,16 @@
+//Inserindo check´s nas req´s http
+import http from 'k6/http';
+import { check } from 'k6'; // Importando o 'check' corretamente
+
+export const options = {
+    vus: 1,
+    duration: '3s'
+}
+
+export default function () {
+    const res = http.get('http://test.k6.io');
+
+    check(res, {
+        'status code é 200': (r) => r.status === 200
+    });
+}
